12dd035b08fe01dfd3407183a34a9f85f46584dc
--add <artiom.co.il>
2024-08-17 20:52:15 +0300

    app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS if your frontend is on a different origin
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://full-stack-app-zza8.vercel.app', // Replace with your frontend URL
+    credentials: true,
+  });
 
 
   await app.listen(3001);
